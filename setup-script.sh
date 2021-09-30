1553e029015ce5806f04801e93b574e2f1272c81
bngu1129 <brandon@barnden.dev>
2021-09-30 18:42:58 -0500

    Update setup-script.sh

--- setup-script.sh
+++ setup-script.sh
@@ -16,9 +16,22 @@ for f in $DATA_PATH/*; do
         # Replace commas with forward slashes to preserve lists
         # Replace tabs with commas to get into CSV format
         # Save to DATA_CLEAN_PATH/filename-clean.csv
-        sed 's/,/\//g' $f | sed 's/\t/,/g' >> ${clean_path}
+        head -n1 $f | sed 's/\t/,/g' | sed 's/.//' > ${clean_path}
+        tail -n+2 $f | sed 's/,/\//g' | awk '{FNR > 1; OFS=","; $1=""; print $0}' | sed 's/^,//g' >> ${clean_path}
     fi
 
     unset filename
     unset clean_path
 done
+
+# Remove duplicates in titleId column in titles.csv
+awk -F"," '!seen[$1]++' $DATA_CLEAN_PATH/titles.csv > $DATA_CLEAN_PATH/temp.csv
+mv $DATA_CLEAN_PATH/temp.csv $DATA_CLEAN_PATH/titles.csv
+
+# Remove duplicates in nconst column in principals.csv
+awk -F"," '!seen[$2]++' $DATA_CLEAN_PATH/principals.csv > $DATA_CLEAN_PATH/temp.csv
+mv $DATA_CLEAN_PATH/temp.csv $DATA_CLEAN_PATH/principals.csv
+
+# Remove duplicates in nconst column
+awk -F"," '!seen[$1]++' $DATA_CLEAN_PATH/names.csv > $DATA_CLEAN_PATH/temp.csv
+mv $DATA_CLEAN_PATH/temp.csv $DATA_CLEAN_PATH/names.csv
