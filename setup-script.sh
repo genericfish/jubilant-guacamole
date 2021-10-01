32c13a472bd686cc3061218e097d2e1f583025ad
bngu1129 <brandon@barnden.dev>
2021-09-30 20:15:31 -0500

    Update setup-script.sh

--- setup-script.sh
+++ setup-script.sh
@@ -17,7 +17,7 @@ for f in $DATA_PATH/*; do
         # Replace tabs with commas to get into CSV format
         # Save to DATA_CLEAN_PATH/filename-clean.csv
         head -n1 $f | sed 's/\t/,/g' | sed 's/.//' > ${clean_path}
-        tail -n+2 $f | sed 's/,/\//g' | awk '{FNR > 1; OFS=","; $1=""; print $0}' | sed 's/^,//g' >> ${clean_path}
+        tail -n+2 $f | sed 's/,/\//g' | awk -F"\t" '{FNR > 1; OFS=","; $1=""; print $0}' | sed 's/^,//g' >> ${clean_path}
     fi
 
     unset filename
@@ -32,6 +32,38 @@ mv $DATA_CLEAN_PATH/temp.csv $DATA_CLEAN_PATH/titles.csv
 awk -F"," '!seen[$2]++' $DATA_CLEAN_PATH/principals.csv > $DATA_CLEAN_PATH/temp.csv
 mv $DATA_CLEAN_PATH/temp.csv $DATA_CLEAN_PATH/principals.csv
 
-# Remove duplicates in nconst column
+# Remove duplicates in nconst column in names.csv
 awk -F"," '!seen[$1]++' $DATA_CLEAN_PATH/names.csv > $DATA_CLEAN_PATH/temp.csv
 mv $DATA_CLEAN_PATH/temp.csv $DATA_CLEAN_PATH/names.csv
+
+# Make sure crew.csv always has enough columns
+awk -F"," '{ if(NF == 2) print $0","; else if(NF == 1) print $0",,"; else print $0; }' $DATA_CLEAN_PATH/crew.csv > $DATA_CLEAN_PATH/temp.csv
+mv $DATA_CLEAN_PATH/temp.csv $DATA_CLEAN_PATH/crew.csv
+
+# Remove all bad data from titles.csv
+# sed 's/,,/,NA,/g' $DATA_CLEAN_PATH/titles.csv > $DATA_CLEAN_PATH/temp.csv
+awk -F"," 'NF == 10 { print $0 }' $DATA_CLEAN_PATH/titles.csv > $DATA_CLEAN_PATH/temp2.csv
+mv $DATA_CLEAN_PATH/temp2.csv $DATA_CLEAN_PATH/titles.csv
+
+
+echo "key,titleId,nconst,category,job,characters" > $DATA_CLEAN_PATH/temp.csv
+awk -F"," 'NR > 1 {print NR-1","$0;}' $DATA_CLEAN_PATH/principals.csv >> $DATA_CLEAN_PATH/temp.csv
+mv $DATA_CLEAN_PATH/temp.csv $DATA_CLEAN_PATH/principals.csv
+
+echo "key,customerId,rating,date,titleId" > $DATA_CLEAN_PATH/temp.csv
+awk -F"," 'NR > 1 {print NR-1","$0;}' $DATA_CLEAN_PATH/customer_ratings.csv >> $DATA_CLEAN_PATH/temp.csv
+mv $DATA_CLEAN_PATH/temp.csv $DATA_CLEAN_PATH/customer_ratings.csv
+
+PSQL_FLAGS="-h csce-315-db.engr.tamu.edu -U csce315_913_4_user -d csce315_913_4_db"
+
+psql -c "CREATE TABLE IF NOT EXISTS crew(titleId TEXT PRIMARY KEY,directors TEXT,writers TEXT);" $PSQL_FLAGS
+psql -c "CREATE TABLE IF NOT EXISTS titles(titleId TEXT PRIMARY KEY,titleType TEXT,originalTitle TEXT, startYear TEXT, endYear TEXT, runtimeMinutes TEXT, genres TEXT, Year TEXT, averageRating TEXT, numVotes TEXT);" $PSQL_FLAGS
+psql -c "CREATE TABLE IF NOT EXISTS customerRatings(key TEXT PRIMARY KEY, customerId TEXT, rating TEXT, date TEXT, titleId TEXT);" $PSQL_FLAGS
+psql -c "CREATE TABLE IF NOT EXISTS names(nconst TEXT,primaryName TEXT, birthYear TEXT, deathYear TEXT, primaryProfession TEXT);" $PSQL_FLAGS
+psql -c "CREATE TABLE IF NOT EXISTS principals(key TEXT PRIMARY KEY, titleId TEXT, nconst TEXT, category TEXT, job TEXT, characters TEXT);" $PSQL_FLAGS
+
+psql -c "\copy crew from 'data/clean/crew.csv' CSV HEADER" $PSQL_FLAGS
+psql -c "\copy titles from 'data/clean/titles.csv' CSV HEADER" $PSQL_FLAGS
+psql -c "\copy customerRatings from 'data/clean/customer_ratings.csv' CSV HEADER" $PSQL_FLAGS
+psql -c "\copy names from 'data/clean/names.csv' CSV HEADER" $PSQL_FLAGS
+psql -c "\copy principals from 'data/clean/principals.csv' CSV HEADER" $PSQL_FLAGS
