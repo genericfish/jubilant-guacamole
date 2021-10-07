b860f9f8cc94b3d3a0d214978319836bb0586c86
Feier Chen <feierchen@Feiers-MacBook.local>
2021-10-07 11:21:17 -0500

    updated GUI

--- TheSQLViewer.java
+++ TheSQLViewer.java
@@ -2,6 +2,9 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.HashMap;
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.BorderLayout;
+
 public class TheSQLViewer {
     // Contains every page
     static HashMap<String, JPanel> gPages = new HashMap<String, JPanel>();
@@ -22,6 +25,7 @@ public class TheSQLViewer {
         // Generate Pages
         generateLandingPage();
         generateViewerPage();
+        generateAnalystPage(); 
 
         frame.add(gBody);
         setPage("landingPage");
@@ -33,13 +37,90 @@ public class TheSQLViewer {
     public static void generateViewerPage()
     {
         JPanel page = new JPanel();
+        
+        // Page components
+        JLabel title = new JLabel("Project 4: The SQL");
+        //page.setLayout(new BorderLayout());
+
+        JButton InProgress = new JButton("In Progess");
+        JButton popular = new JButton("Popular");
+        JButton New = new JButton("New");
+        JButton likes = new JButton("Likes");
+        JButton Dislikes = new JButton("Dislikes"); 
+        JTextField search= new JTextField("Search", 20);
+
+        String[][] rec = {
+         { "1", "Steve", "AUS", " ", " " },
+         { "2", "Virat", "IND", " ", " "},
+         { "3", "Kane", "NZ", " ", " " },
+         { "4", "David", "AUS", " ", " " },
+         { "5", "Ben", "ENG", " "," " },
+         { "6", "Eion", "ENG", " "," " },
+        };
+        String[] header = { "Name", "Genre", "Year", "Runtime", " " };
+        JTable table = new JTable(rec, header);
+
+        page.add(title);
+        page.add(InProgress);
+        page.add(popular);
+        page.add(New);
+        page.add(likes);
+        page.add(Dislikes); 
+        page.add(search); 
+        page.add(new JScrollPane(table));
+
+        gPages.put("viewerPage", page);
+    }
+
+    public static void generateAnalystPage()
+    {
+        JPanel page = new JPanel();
 
         // Page components
         JLabel title = new JLabel("Project 4: The SQL");
 
         page.add(title);
 
-        gPages.put("viewerPage", page);
+        JButton content = new JButton("Content");
+        JButton director = new JButton("Directors");
+        JButton actor = new JButton("Actors");
+        JTextField search= new JTextField("Search", 20);
+
+        page.add(content);
+        page.add(director);
+        page.add(actor); 
+        page.add(search);
+
+        JLabel tabletitle = new JLabel("Popular Right Now");
+
+        String[][] rec1 = {
+         { "1", "Steve", "AUS" },
+         { "2", "Virat", "IND"},
+         { "3", "Kane", "NZ"},
+         { "4", "David", "AUS" },
+         { "5", "Ben", "ENG"},
+         { "6", "Eion", "ENG"},
+        };
+        String[] header1 = { "Title", "stuff", "Popularity" };
+        JTable table1 = new JTable(rec1, header1);
+
+        String[][] rec2 = {
+         { "1", "Steve", "AUS" },
+         { "2", "Virat", "IND"},
+         { "3", "Kane", "NZ"},
+         { "4", "David", "AUS"},
+         { "5", "Ben", "ENG"},
+         { "6", "Eion", "ENG"},
+        };
+        String[] header2 = { "Genre", "Directors", "Actors"};
+        JTable table2 = new JTable(rec2, header2);
+
+        page.add(new JScrollPane(table1));
+        page.add(tabletitle); 
+        page.add(new JScrollPane(table2));
+
+        gPages.put("analystPage", page);
+        
     }
 
     // Generates the page which the user first encounters
@@ -55,6 +136,8 @@ public class TheSQLViewer {
         viewer.addActionListener(e -> setPage("viewerPage"));
 
         JButton analyst = new JButton("Content Analyst");
+
+        analyst.addActionListener(e -> setPage("analystPage"));
 
         page.add(title);
         page.add(viewer);
