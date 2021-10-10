be5f2cfdc9e6f1ba25e2a6e41cd0f1cfb82011d0
barnden <brandon@barnden.dev>
2021-10-10 13:20:35 -0500

    GUI: Properly align elements on login page

--- TheSQLLogin.java
+++ TheSQLLogin.java
@@ -1,6 +1,6 @@
 import java.awt.*;
-import javax.swing.*;
 import java.sql.*;
+import javax.swing.*;
 
 public class TheSQLLogin extends Page {
     public TheSQLLogin()
@@ -12,37 +12,42 @@ public class TheSQLLogin extends Page {
         JLabel prompt = new JLabel("Please Enter Customer ID");
         prompt.setFont(TheSQL.gHeaderFont);
 
-        JLabel title = new JLabel("  " + TheSQL.gGroupName);
+        JLabel title = new JLabel(TheSQL.gGroupName);
         title.setFont(TheSQL.gHeaderFont);
-        JTextField customerID = new JTextField("1488844");
-        JButton loginButton = new JButton("Login");
+        title.setHorizontalAlignment(SwingConstants.CENTER);
+
         JLabel loginError = new JLabel("Username does not exist");
         loginError.setVisible(false);
         loginError.setForeground(Color.RED);
+        loginError.setHorizontalAlignment(SwingConstants.CENTER);
 
-        loginButton.addActionListener(e -> {
+        JTextField customerID = new JTextField("1488844");
+        JButton loginButton = new JButton("Login");
+        loginButton.addActionListener(event -> {
             TheSQL.gUsername = customerID.getText();
+
             ResultSet userExists = TheSQL.gDatabase.query(
-                String.format("SELECT customerid FROM customerratings WHERE customerid='%s';",TheSQL.gUsername)
-            );
+                String.format(
+                    "SELECT customerid FROM customerratings WHERE customerid='%s';",
+                    TheSQL.gUsername));
 
-            try{
-                if(userExists.next()){
+            try {
+                if (userExists.next()) {
                     TheSQL.setPage("viewer");
-                }
-                else{
-                    System.out.println("Name doesn't work.");
-                    loginError.setVisible(true);
-                }
-            }
-            catch(Exception d){d.printStackTrace();}
 
+                    return;
+                }
+
+                loginError.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         });
 
-        add(title, 20, 2, 3, 0);
-        add(prompt, 0, 5, 1, 1);
-        add(customerID, 1, 10, 2, 2);
-        add(loginButton, 2, 15, 3, 3);
-        add(loginError, 3, 20, 4, 4);
+        add(title, 20, 20, 0, 0);
+        add(prompt, 0, 20, 0, 1);
+        add(customerID, 10, 20, 0, 2);
+        add(loginButton, 10, 20, 0, 3);
+        add(loginError, 10, 20, 0, 4);
     }
 }
