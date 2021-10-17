c5d8b85f69fba99bd7086ae11def02b133bc2d3c
barnden <brandon@barnden.dev>
2021-10-17 13:34:37 -0500

    Analyst: Properly close scanner in IndirectDirector

--- IndirectDirector.java
+++ IndirectDirector.java
@@ -1,26 +1,26 @@
-import java.io.File;   
-import java.util.Scanner;   
+import java.io.File;
 import java.sql.*;
+import java.util.Scanner;
 
-public class IndirectDirector  
-{     
-    public static ResultSet getDirector(String actor)  
-    {  
-        try  
-        {  
-            //constructor of file class having file as argument  
-            File file=new File("Indirect_Director_SQL.sql");   
-            Scanner sc = new Scanner(file);     //file to be scanned  
+public class IndirectDirector {
+    public static ResultSet getDirector(String actor)
+    {
+        ResultSet result = null;
+        try {
+            //constructor of file class having file as argument
+            File file = new File("Indirect_Director_SQL.sql");
+            Scanner sc = new Scanner(file); //file to be scanned
             String query = "";
-            while (sc.hasNextLine()) {       //returns true if and only if scanner has another token  
-                query += sc.nextLine();  
+
+            while (sc.hasNextLine()) { //returns true if and only if scanner has another token
+                query += sc.nextLine();
             }
-            return TheSQL.gDatabase.query(String.format(query,actor)); 
-        }  
-        catch(Exception e)  
-        {  
-        e.printStackTrace();  
-        }  
-        return null;
-    }  
-}  
+
+            result = TheSQL.gDatabase.query(String.format(query, actor));
+            sc.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
